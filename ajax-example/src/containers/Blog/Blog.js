070b2752a3da5ee5c79d0372b56ca2b26116e480
Helene Lemascon <lemasson.h@gmail.com>
2018-01-30 09:14:38 +0000

    Ajax example - Learn how to lazy load Component

--- ajax-example/src/containers/Blog/Blog.js
+++ ajax-example/src/containers/Blog/Blog.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
+import asyncComponent from '../../hoc/asyncComponent';
 import Posts from '../Posts/Posts';
-import NewPost from '../NewPost/NewPost';
+// import NewPost from '../NewPost/NewPost';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import './Blog.css';
 
+const AsyncNewPost = asyncComponent(() => {
+  return import('../NewPost/NewPost');
+});
+
 class Blog extends Component {
+    state = {
+        auth: true,
+    };
+
     render () {
         return (
             <div className="Blog">
@@ -29,9 +38,10 @@ class Blog extends Component {
                   </nav>
                 </header>
                 <Switch>
-                  <Route path="/new-post" component={NewPost} />
+                  {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null }
                   <Route path="/posts" component={Posts} />
-                  <Redirect from="/" to="/posts" />
+                  <Redirect from="/" exact to="/posts" />
+                  <Route render={() => <h1>Not found</h1>} />
                   {/* <Route path="/" component={Posts} /> */}
                 </Switch>
                 {/* <Route path="/" render={() => <h1>Home</h1>}/>
