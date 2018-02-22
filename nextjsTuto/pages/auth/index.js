65b3f8fbe8db6f51b069cbf4d3b04984bffb9157
Helene Lemascon <lemasson.h@gmail.com>
2018-02-22 20:54:31 +0000

    Nextjs Tuto - examples of getInitialProps lifecycle

--- nextjsTuto/pages/auth/index.js
+++ nextjsTuto/pages/auth/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 
 import User from '../../components/User';
 
-const authIndexPage = () => (
-  <div>
-    <h1>The Auth Index Page</h1>
-    <User name="Helene" age="26" />
-  </div>
-);
+const authIndexPage = (props) => {
+  return (
+    <div>
+      <h1>The Auth Index Page - {props.appName}</h1>
+      <User name="Helene" age="26" />
+    </div>
+  );
+};
+
+authIndexPage.getInitialProps = async (context) => {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({appName: 'Super App (Auth)'});
+      }, 1000);
+    });
+
+    return promise;
+}
 
 export default authIndexPage;
