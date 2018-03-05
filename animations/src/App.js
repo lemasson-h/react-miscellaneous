2a7850426aa7160b92fc088576feffc3bc464830
Helene Lemascon <lemasson.h@gmail.com>
2018-03-05 16:42:54 +0000

    Animation - Move transition to Modal component + change timeout for enter/exit

--- animations/src/App.js
+++ animations/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Transition from 'react-transition-group/Transition';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -28,15 +27,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Transition
-          in={this.state.modalIsOpen}
-          mountOnEnter
-          unmountOnExit
-          timeout={300}>
-          {state => (
-            <Modal show={state} closed={this.closeModal}/>
-          )}
-        </Transition>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
         <Backdrop show={this.state.modalIsOpen}  />
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
